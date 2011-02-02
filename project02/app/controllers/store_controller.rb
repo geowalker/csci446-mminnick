26ea7192c6413ecbd334fd4f192e97b26f9d05d7
Matthew Minnick <mminnick@mines.edu>
2011-02-02 00:00:02 -0700

    Implemented AJAX on cart with some cool javascript effects and used a helper method

--- project02/app/controllers/store_controller.rb
+++ project02/app/controllers/store_controller.rb
@@ -7,9 +7,10 @@ class StoreController < ApplicationController
   def add_to_cart
     product = Product.find(params[:id])
     @cart = find_cart
-    @cart.add_product(product)
+    @current_item = @cart.add_product(product)
     respond_to do |format|
-    	 format.js
+    	 format.js if request.xhr?
+    	 format.html {redirect_to_index}
     end
   rescue ActiveRecord::RecordNotFound
     logger.error("Attempt to access invalid product #{params[:id]}")
@@ -21,7 +22,7 @@ class StoreController < ApplicationController
   
   def empty_cart
     session[:cart] = nil
-    redirect_to_index("Your cart is currently empty")
+    redirect_to_index
   end
 
 
